56a2d7b2f73f60442db3fdbc17e2f286812453f8
BJS324MC <bjs332244@gmail.com>
2021-12-04 01:45:08 +0800

    yes

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const cors = require('cors'),
       app = express();
 app.use(cors());
 app.get('/:id/:type', async (req, res) => {
-  try {
-    const { id, type } = req.params;
-    if (type === '0') {
-      res.setHeader('Content-Disposition',`attachment; filename=${id}.mp3`);
-      ytdl(baseURL + id, {filter: format => format.container === 'm4a' && !format.encoding,quality: 'highest'}).pipe(res);
-    } else if (Number(type) < 3 && Number(type) > -1){
-      res.header('Content-Disposition',`attachment; filename="${id}.mp4"`);
-      ytdl(baseURL + id, {filter: type === '1' ? 'videoonly' : 'audioandvideo',quality:'highestvideo'}).pipe(res);
-    }} catch (e) {console.log(e);}
+  const { id, type } = req.params;
+  if (type === 'audio') {
+    res.setHeader('Content-Disposition',`attachment; filename=${id}.mp3`);
+    ytdl(baseURL + id, {filter: 'audioonly',quality: 'highest'}).pipe(res);
+  } else if (type === 'video'){
+    res.header('Content-Disposition',`attachment; filename="${id}.mp4"`);
+    ytdl(baseURL + id, {filter: 'videoonly',quality:'highestvideo'}).pipe(res);
+  } else res.send('400 Invaild format type.');
+});
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  res.header('Content-Disposition',`attachment; filename="${id}.mp4"`);
+  ytdl(baseURL + id, {filter: 'audioandvideo',quality:'highestvideo'}).pipe(res);
 });
 
 const port = process.env.PORT || 3000;
